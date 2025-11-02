14feab6415074021aa38041d1c0ba3a5bf252294
rembo17 <rembo172009@gmail.com>
2025-11-02 21:27:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8585282471:AAEnwbXz0Y6ueWqNtAetUHFh-lRBoN4zEQk",
+  owner: "8261602791",
+  port: "23340",
+  ipvps: "https://kotepporeper-lovemyta.kandigpanel.my.id"
+};
